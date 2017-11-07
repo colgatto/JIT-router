ad9da7cd6583494ca58a63d577bf1bff2151f546
colgatto <colgatto@protonmail.com>
2017-11-07 17:52:14 +0100

    add template json

--- server/router.js
+++ server/router.js
@@ -1,13 +1,18 @@
-var path = require('path');
-var url = require('url');
+const path = require('path');
+const url = require('url');
+const sitemap = require('./sitemap');
+//const util = require('util');
+
 var headers = {
 	"access-control-allow-origin": "*",
 	"access-control-allow-methods": "GET, POST",
 	//"access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
 	"access-control-allow-headers": "content-type, accept",
 	//"access-control-max-age": 10,
-	"Content-Type": "application/json"
+	//"Content-Type": "application/json"
 };
+
+console.log(JSON.stringify(sitemap, null, 2));
 
 prepareResponse = function(req, cb){
 	var data = "";
@@ -17,29 +22,21 @@ prepareResponse = function(req, cb){
 
 respond = function(res, data, status = 200) {
 	res.writeHead(status, headers);
-	res.end(data);
+	res.write(data);
+	res.end();
 }
 
 send404 = function(res){
 	respond(res, 'Not Found', 404);
 }
 
-redirector = function(res, loc, status = 302){
+redirector = function(res, loc, status = 307){
 	res.writeHead(status, { Location: loc });
 	res.end();
 }
 
 var actions = {
 	'GET': function(req, res) {
-		/* if your router needs to pattern-match endpoints
-		 * var parsedUrl = url.parse(req.url);
-		 * var endPoint = parsedUrl.pathname === '/' ? '/index.html' : parsedUrl.pathname;
-		*/
-		/*
-		 * DO SOMETHING - get asset, query database, etc. -> store as `data`
-		 * pass the result of that operation as data into responder -> store result status code as `statusCode`
-		 * pass the status code into responder
-		*/
 		var data = {};
 		var minRes = url.parse(req.url);
 		data.route = minRes.pathname.split('/');
@@ -52,22 +49,28 @@ var actions = {
 			}, {});
 		}
 		console.log(data);
-		respond(res, "fatto");
+		if(data.route[0] === "index")
+			redirector(res,"/public/index.html");
+		else
+			respond(res, "<div>fatto</div>");
 	},
 	'POST': function(req, res) {
 		prepareResponse(req, function(data) {
-			// Do something with the data that was just collected by the helper
-			// e.g., validate and save to db
-			// either redirect or respond
-			// should be based on result of the operation performed in response to the POST request intent
-			// e.g., if user wants to save, and save fails, throw error
 			//redirector(res, /* redirect path , optional status code -	defaults to 302 */);
-			respond(res, data);
+			//respond(res, data);
 		});
 	}
 }; 
 
 exports.handleRequest = function(req, res) {
+	/**/
+	if(req.url === '/favicon.ico'){
+		res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+		res.end();
+		console.log('favicon requested');
+		return;
+	}
+	/**/
 	var action = actions[req.method];
 	action ? action(req, res) : send404(res);
 };
